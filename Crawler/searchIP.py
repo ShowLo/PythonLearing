c473200a5f007f8dd8f3ee8e8d6fd93b6c825d73
ShowLo <945519267@qq.com>
2017-04-11 13:08:16 +0800

    add the project of

---
+++ Crawler/searchIP.py
@@ -0,0 +1,18 @@
+﻿import requests
+import re
+def searchIP(IP):
+    try:
+        url = 'http://www.ip138.com/ips138.asp?ip=' + IP;                       #用来查询IP地址的url
+        kv = {'user-agent':'Mozila/5.0'};                                       #模仿浏览器行为
+        r = requests.get(url,timeout = 30,headers = kv);
+        r.raise_for_status();
+        if r.encoding == 'ISO-8859-1':
+            r.encoding = r.apparent_encoding;
+        address = re.search('<li>本站数据：(?P<realAddress>.+?)</li>',r.text);  #找到存放IP地址的数据所在
+        return address.group('realAddress');
+
+    except:
+        print('exception!');
+
+IP = '59.66.141.44';
+print(searchIP(IP));
