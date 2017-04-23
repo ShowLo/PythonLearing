211c99f21a2eb1463d699241621ac6eedef623f6
ShowLo <945519267@qq.com>
2017-04-23 13:17:30 +0800

    add notes

--- Crawler/getUniversityRanking.py
+++ Crawler/getUniversityRanking.py
@@ -1,4 +1,4 @@
-﻿# -*- coding:utf-8 -*-
+﻿﻿# -*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import bs4
@@ -9,17 +9,17 @@ def getHTMLText(url):
         r = requests.get(url,timeout = 30,headers = kv);
         r.raise_for_status();
         if r.encoding == 'ISO-8859-1':
-            r.encoding = r.apparent_encoding;
+            r.encoding = r.apparent_encoding;                                       #改为正确的编码
         return r.text;
     except:
         return ''
 
 def fillUniversityList(ulist,html):
     soup = BeautifulSoup(html,'lxml');
-    for tr in soup.find('tbody').children:
+    for tr in soup.find('tbody').children:                                          #找到放置排名信息的地方
         if isinstance(tr,bs4.element.Tag):
             tds = tr('td');
-            ulist.append([tds[0].string,tds[1].string,tds[3].string]);
+            ulist.append([tds[0].string,tds[1].string,tds[3].string]);              #加入排名、学习名称和总分信息
     
 def printUlist(ulist,num):
     print('{:^10}\t{:^10}\t{:^10}'.format('排名','学校名称','总分'))
